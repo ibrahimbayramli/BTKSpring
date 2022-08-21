aa34051f7bcec31ec66d3e242070de415f5e6ccf
İbrahim BAYRAMLI <ibrahimbayramli1902@gmail.com>
2022-08-22 01:25:01 +0300

    BTKSpring

---
+++ src/main/java/com/ibrahimbayramli/btkDemo/BtkDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ibrahimbayramli.btkDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtkDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtkDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ibrahimbayramli/btkDemo/BtkDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibrahimbayramli.btkDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtkDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
